58b01f88ba153f97604f92fb7118ca26207f6fa1
ekatrif <katiapervak@gmail.com>
2023-02-03 11:20:07 +0100

    feat: task is done

--- src/index.js
+++ src/index.js
@@ -38,8 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let output = "";
+    let oneLetter="";
+    for (let i = 0; i < expr.length; i+=10) {
+      //Взять кусок строки
+      oneLetter = expr.slice(i, i+10);
+      console.log(oneLetter);
+      //отдельно обработать пробел **********
+      if (oneLetter === "**********") {
+        output += " ";
+        continue;
+      }
+      //преобразовать его в точка-тире
+      let currentDotOrDash = [];
+      let lastTwoNumbers="";
+      for (let j = oneLetter.length - 1; j >= 0; j-=2 ) {
+        lastTwoNumbers = oneLetter.slice(j - 1, j + 1);
+        if (lastTwoNumbers === "11") {
+          currentDotOrDash.push("-");
+        } else if (lastTwoNumbers === "10") {
+          currentDotOrDash.push(".");
+        }
+      }
+      //по таблице найти букву
+      output += MORSE_TABLE[currentDotOrDash.reverse().join("")];
+    }
+    return output;
+  }
 
 module.exports = {
     decode
